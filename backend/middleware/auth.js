ede5ee3ed1123f40ac7f8366086e62abdfc6030e
Hippo <hippolytehamonet@macbook-pro-de-hippo.home>
2021-06-21 14:02:26 +0200

    création dossier backend et structure backend, création dossier frontend

---
+++ backend/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+
+// changer la clé random token qui est trop utilisée et facilement trouvable
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        req.token = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+            next()
+    } catch (error) {
+        res.status(401).json({
+            error: error
+        })
+    }
+
+};
